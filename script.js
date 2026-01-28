d26f85db2876907300ddc69d46bdeed4136c4b89
AZAMGURUYT <112607208+AZAMGURUYT@users.noreply.github.com>
2026-01-28 16:03:31 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// HERO text
+gsap.from(".hero h1", {
+  y: 80,
+  opacity: 0,
+  duration: 1.4,
+  ease: "power4.out"
+});
+
+gsap.from(".hero p", {
+  y: 40,
+  opacity: 0,
+  delay: 0.4,
+  duration: 1,
+  ease: "power3.out"
+});
+
+// Split sections
+gsap.utils.toArray(".split").forEach(section => {
+  gsap.from(section.querySelector(".text"), {
+    scrollTrigger: {
+      trigger: section,
+      start: "top 80%",
+    },
+    x: -80,
+    opacity: 0,
+    duration: 1.2,
+    ease: "power3.out"
+  });
+
+  gsap.from(section.querySelector(".image"), {
+    scrollTrigger: {
+      trigger: section,
+      start: "top 80%",
+    },
+    x: 80,
+    opacity: 0,
+    duration: 1.2,
+    ease: "power3.out"
+  });
+});
